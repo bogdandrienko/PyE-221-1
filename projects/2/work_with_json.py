d108cde2e8c808c2a4ffa1ad1c7ea0ee1cfca7a7
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2022-05-30 22:31:00 +0600

    update

--- projects/2/work_with_json.py
+++ projects/2/work_with_json.py
@@ -28,8 +28,8 @@ var_dict4 = {
 print(type(var_dict4))
 
 url = "https://jsonplaceholder.typicode.com/posts/1"
-response = requests.get(url)
-json_data = response.content.decode()
+response = requests.get(url)  # Объект библиотеки
+json_data = response.content.decode()  # строка
 print(json_data)
 
 with open('data.json', 'w') as file:
