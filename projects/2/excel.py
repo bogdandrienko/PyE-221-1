5853dd32584a6416d657050402184ccdf6b77a40
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2022-05-30 21:11:46 +0600

    update

--- projects/2/excel.py
+++ projects/2/excel.py
@@ -4,7 +4,6 @@
 from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
 
-
 # создание "объекта" из библиотеки openpyxl
 workbook = Workbook()
 
@@ -13,6 +12,26 @@ worksheet = workbook.active
 
 var_list = ["Almaty", "Nur-Sultan", "Taraz", "Ekibastuz"]
 
+# ["Almaty", "Nur-Sultan", "Taraz", "Ekibastuz"]
+# ["Almaty", "Nur-Sultan", "Taraz", "Ekibastuz"]
+# ["Almaty", "Nur-Sultan", "Taraz", "Ekibastuz"]
+# ["Almaty", "Nur-Sultan", "Taraz", "Ekibastuz"]
+
+var_range = range(1, 1000, 1)
+# print(var_range)
+# for n in var_range:
+#     print(n)
+
+# функция range возвращает массив чисел
+# x = range(6+1)
+# for n in x:
+#     # print(n)
+#     pass
+
+# x = range(3, 200, 2)
+# for n in x:
+#     print(n)
+
 # "кривой способ решения задачи"
 # for j in "ABCD":
 #     row = "1"
@@ -20,13 +39,14 @@ var_list = ["Almaty", "Nur-Sultan", "Taraz", "Ekibastuz"]
 #     # записываем значение в выбранную ячейку
 #     worksheet[f'{col}{row}'] = str(var_list["ABCD".index(j)])
 
-for name in var_list:
-    row = "1"
-    # 1 -> A, 3 -> C, 26 -> Z
-    col = get_column_letter(var_list.index(name)+1)
-    # записываем значение в выбранную ячейку
-    worksheet[f'{col}{row}'] = str(name)
-
+# пока вложенный цикл не отработает полностью, вторая итерация верхнего цикла не запускается
+for num in var_range:
+    for name in var_list:
+        row = num
+        # 1 -> A, 3 -> C, 26 -> Z
+        col = get_column_letter(var_list.index(name) + 1)
+        # записываем значение в выбранную ячейку
+        worksheet[f'{col}{row}'] = str(name)
 
 # записываем значение в выбранную (А1) ячейку
 # worksheet['A1'] = 42
