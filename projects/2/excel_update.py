615a3f578bd796fc60d44b1390573902e8a1ecfa
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2022-06-01 20:23:35 +0600

    update

--- projects/2/excel_update.py
+++ projects/2/excel_update.py
@@ -1,4 +1,6 @@
 import openpyxl
+from openpyxl import Workbook
+from openpyxl.utils import get_column_letter
 
 # чтение
 # обрабатываем данные
@@ -11,15 +13,68 @@ workbook = openpyxl.load_workbook("""temp/sample_example.xlsx""")
 worksheet = workbook.active
 
 # берём последнюю строку в excel-файле
-max_row = worksheet.max_row + 1
+max_row = worksheet.max_row
 print(max_row)
-print(type(max_row))
 # берём последнюю колонку в excel-файле
-max_column = worksheet.max_column + 1
+max_column = worksheet.max_column
 print(max_column)
-print(type(max_column))
+
+# записать всё в массив с массивами [["Almaty", "Nur-Sultan", "Taraz", "Ekibastuz"], ["Almaty", "Nur-Sultan", "Taraz", "Ekibastuz"]...]
+external_array = []
+
+# наполнение внешнего массива
+for row in range(1, max_row + 1):
+    internal_array = []
+
+    # наполнение внутреннего массива
+    for col in range(1, max_column + 1):
+        value = worksheet.cell(row=row, column=col).value
+        if value is None:
+            value = ''
+            # continue  # (продолжить) оператор позволяет пропустить эту итерацию цикла
+            # break  # (прекратить) оператор позволяет полностью остановить цикл
+        internal_array.append(value)
+    # завершение наполнение внутреннего массива
+
+    if len(internal_array) < 1:
+        continue
+
+    external_array.append(internal_array)
+# завершение наполнение внешнего массива
+
+print(external_array)
+
+# index = 0
+# while True:
+#     index += 1
+#     if index > 50:
+#         break # (прекратить) оператор позволяет полностью остановить цикл
+
+workbook_new = Workbook()
+worksheet_new = workbook_new.active
+
+# col_count = external_array[0]
+print(
+    f"col_count: {external_array}")  # [['Almaty', 'Nur-Sultan', 'Taraz', 'Ekibastuz', ''], ['Almaty', 'Nur-Sultan', 'Taraz', 'Ekibastuz', ''],]
+print(f"col_count: {external_array[0]}")  # ['Almaty', 'Nur-Sultan', 'Taraz', 'Ekibastuz', '']
+print(f"col_count: {external_array[0][1]}")  # 'Almaty'
+print(f"col_count: {external_array[0][1][2:-2:1]}")  # 'ma'
 
 
-for row in range(1, max_row):
-    for col in range(1, max_column):
-        value = worksheet.cell(row=i, column=j).value
+def function_len_array(array):  # ['Almaty', 'Nur-Sultan', 'Taraz', 'Ekibastuz', '']
+    return len(array)  # 5
+
+
+for row in range(0, function_len_array(external_array)):  # [0, 1, 2, 3, 4, ..., 1007]
+    # print(f"col_count: {len(external_array[row])}")
+    for col in range(0, function_len_array(external_array[
+                                row])):  # [0, 1, 2, 3, 4, 5]  # external_array[row] == ['Almaty', 'Nur-Sultan', 'Taraz', 'Ekibastuz', '']
+
+        col_letter = get_column_letter(col + 1)
+        print(col_letter)
+        worksheet[f'{col_letter}{row + 1}'] = ''
+
+        # worksheet_new.cell(row=row, column=col) =
+        pass
+    # print(external_array[row])
+    pass
