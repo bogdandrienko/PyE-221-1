2a14a8ed5e4a3b93e8d9cfe45c2e3a395f91c82f
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2022-06-08 19:37:06 +0600

    update

---
+++ projects/3/delete.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+
+# first = os.path.abspath(os.path.dirname(__file__))  # содержит абсолютный путь к текущему скрипту
+first = ''  # содержит относительный путь к текущему скрипту
+second = "temp\\junk2.txt"  # \ - изоляция символа   \n - перенос строки, \t - табуляция...
+path = os.path.join(first, second)
+print(f"path: {path}")
+try:
+    os.remove(path)  # удаление файла
+except Exception as error:
+    print(error)
+finally:
+    try:
+        os.rmdir('temp')  # удаление пустой папки
+    except Exception as error:
+        print(error)
+        shutil.rmtree('temp')  # удаление не пустой папки
+
