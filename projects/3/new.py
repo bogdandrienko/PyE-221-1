06b179e36c000c5ea6c24465e89fd10351740cda
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2022-06-20 22:36:12 +0600

    update

---
+++ projects/3/new.py
@@ -0,0 +1,15 @@
+
+ran1 = range(0, 10)
+for i in ran1:
+    print(i)
+
+new = [
+    ["A1", "A2", "A3"],
+    ["B1", "B2", "B3"],
+    ["C1", "C2", "C3"]
+]
+
+for i in range(0, 2+1):
+    print(new[0][i])
+    print(new[1][i])
+    print(new[2][i])
---
+++ projects/3/set.py
@@ -0,0 +1,50 @@
+import datetime
+
+
+list1 = [1, 2, 3, 5, 7, "123"]
+print(list1)
+print(type(list1))
+
+set1 = set(list1)  # несортированный массив с уникальными значениями
+set2 = {1, 2, 3, 5, 7, "123"}
+print(set1)
+print(type(set1))
+
+set1.add(1111111)  # добавление элемента
+print(set1)
+
+print("\n\n\n\n\n************\n\n\n\n\n")
+
+
+set3 = set(list1)
+set3.add("2222222222")
+print(set3)
+set4 = set(list1)
+set4.add("333333333333")
+print(set4)
+
+set5 = set3.difference(set4)  # разница
+print(set5)
+
+set6 = set3.intersection(set4)  # пересечения (совпадения)
+print(set6)
+
+set7 = set3.union(set4)  # сложение двух множеств
+print(set7)
+
+set8 = set(list1)
+set8.add(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+print(type(datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")))
+print(set8)
+set8.remove(5)  # удаление элемента
+print(set8)
+
+val1 = [x for x in set8 if isinstance(x, str)]  # генератор (list compehansions)
+val2 = []
+for x in set8:
+    if isinstance(x, str):
+        val2.append(x)
+    else:
+        pass
+print(val1)
+print(val2)
