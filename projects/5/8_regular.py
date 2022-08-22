d432d3145e9f825cb9c443b2c5988365f2c8e155
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2022-08-22 22:11:36 +0600

    update

---
+++ projects/5/8_regular.py
@@ -0,0 +1,91 @@
+import hashlib
+
+txt = r"(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
+txt2 = r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
+
+input_from_user1 = "qwerty"
+input_from_user1.isascii()  # пароль должен быть на английском
+input_from_user1.isascii()  # пароль должен сод
+
+# Минимум восемь символов, минимум одна заглавная буква, одна строчная буква и одна цифра:
+
+input_from_user2 = "12345qwerty"
+
+# a = input("124124?")
+
+r"^   (?=.*[a-z])  (?=.*[A-Z])  (?=.*\d)   [A-Z a-z 0-9 \d]  {8,16}  $"
+b = "124124"
+# Хотя бы одна заглавная английская буква ,(?=.*?[A-Z])
+# Хотя бы одна строчная английская буква,(?=.*?[a-z])
+# Хотя бы одну цифру,(?=.*?[0-9])
+# По крайней мере, один специальный символ,(?=.*?[#?!@$%^&*-])
+# Минимум восемь в длину .{8,}(с анкерами)
+
+# @ .com / .ru
+
+import re
+
+regex = r'^       [a-z0-9] + [\._]?[a-z0-9]    + [@]\w  + [.]\w    {2,3}       $'
+
+
+def check(email):
+    our_regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    email = email
+    result = re.search(pattern=our_regex, string=email)
+
+    print(result)
+    if re.search(r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$', email):
+        print("Почта верная")
+    else:
+        print("Почта не верная, повторите")
+
+
+# либо бесконечный цикл, либо рекурсия
+
+check('bogdanrienko@gmail.com')
+
+
+# пароль
+
+def sum1(a2: int, b2: int) -> int:
+    return a2 + b2
+
+
+def summary() -> None:
+    try:
+        a1 = input("Введите число а1:")  # python
+        b1 = input("Введите число b1:")  # python
+        print(sum1(int(a1), int(b1)))
+    except:
+        print("\n\nОшибка ввода, повторите ввод!\n")
+        summary()
+
+
+# summary()
+
+# while True:
+#     # isinstance(1.0, int)
+#     # type(1) ==
+#     a1 = input("Введите число а1:")  # python
+#     if not re.search(r'^\d+$', a1):
+#         print("\n\nОшибка ввода, повторите ввод!\n")
+#         continue
+#     b1 = input("Введите число b1:")  # python
+#     if not re.search(r'^\d+$', b1):
+#         print("\n\nОшибка ввода, повторите ввод!\n")
+#         continue
+#     print(sum1(int(a1), int(b1)))
+#     break
+
+while True:
+    a1 = input("Введите новый пароль:")  # python
+    if not re.search(r"^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$", a1):
+        print("\n\nОшибка ввода, повторите ввод!\n")
+        continue
+    b1 = input("Введите новый пароль ещё раз:")  # python
+    if a1 != b1:
+        print("\n\nОшибка ввода, повторите ввод!\n")
+        continue
+    print(hashlib.sha512(b1.encode()))
+    break
+
---
+++ projects/5/9_threading.py
@@ -0,0 +1,115 @@
+import time
+import threading
+import multiprocessing
+import asyncio
+
+
+def decor(func):
+    def decor_inline(*args, **kwargs):  # *(12, 15, 6,) - tuple(кортеж) \ **{"name":"Py", 'age': 12} - dict(словарь)
+        # ПРЕДОБРАБОТКА
+        time_start = time.perf_counter()
+        # ПРЕДОБРАБОТКА
+
+        # arg1 = args[1]
+
+        result = func(*args, **kwargs)
+
+        # ПОСТОБРАБОТКА
+        print(f'всё действие заняло: {round(time.perf_counter() - time_start, 5)}')
+        # ПОСТОБРАБОТКА
+
+        return result
+
+    return decor_inline
+
+
+def task(name: str) -> None:
+    print(f'начало {name}')
+    time.sleep(1.0)  # блокирующая операция - обращение в базу данных, загрузка картинки...
+    print(f'конец {name}')
+
+
+async def task_a(name: str) -> None:
+    print(f'начало {name}')
+    await asyncio.sleep(1.0)  # блокирующая операция - обращение в базу данных, загрузка картинки...
+    print(f'конец {name}')
+
+
+# последовательная
+@decor
+def synhronus():  # 3.01685
+    task(name='Задача 1')
+    task(name='Задача 2')
+    task(name='Задача 3')
+
+
+# многопоточность
+@decor
+def threadings():  # 1.01568
+    new_thread_1 = threading.Thread(target=task, args=('Задача 1',))
+    new_thread_1.start()
+
+    new_thread_2 = threading.Thread(target=task, args=('Задача 2',))
+    new_thread_2.start()
+
+    new_thread_3 = threading.Thread(target=task, args=('Задача 3',))
+    new_thread_3.start()
+
+    new_thread_1.join()
+    new_thread_2.join()
+    new_thread_3.join()
+
+    print('я главный поток, я закончил работу, я домой')
+
+
+# мультипроцессорность
+@decor
+def processings():  # 1.10279
+    new_process_1 = multiprocessing.Process(target=task, args=('Задача 1',))
+    new_process_1.start()
+
+    new_process_2 = multiprocessing.Process(target=task, args=('Задача 2',))
+    new_process_2.start()
+
+    new_process_3 = multiprocessing.Process(target=task, args=('Задача 3',))
+    new_process_3.start()
+
+    new_process_1.join()
+    new_process_2.join()
+    new_process_3.join()
+
+    print('я главный поток, я закончил работу, я домой')
+
+
+# асинхронная
+@decor
+def asynchronus():
+    async def task_generator():
+        await asyncio.gather(*[
+            task_a(name='Задача 1'),
+            task_a(name='Задача 2'),
+            task_a(name='Задача 3'),
+        ])
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(task_generator())
+
+
+if __name__ == "__main__":
+    # synhronus()    # 3.016   1 процесс и 1 поток, но блокирующие
+    # threadings()   # 1.015   1 процесс и много потоков
+    # processings()  # 1.102   много процессов и 1 поток (на самом деле много)
+    # asynchronus()    # 1.003   1 процесс и 1 поток, но не блокирующие
+
+    # # *(12, 15, 6,) - tuple(кортеж) \ **{"name": "Py", 'age': 12} - dict(словарь)
+
+    # tup1 = (12, 15, 6,)
+    # print(*tup1)
+
+    def tas(name, age):
+        print(name, age)
+
+    dict1 = {"name": "Py", 'age': 12}
+    # tas(name=dict1["name"], age=dict1['age'])
+    tas(**dict1)
+    # print(name="Py", age=12) # **dict1
