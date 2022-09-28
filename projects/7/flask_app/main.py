f31580206577e0f93a9abd18c9f876c7875bf977
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2022-09-28 20:25:09 +0600

    update

--- projects/7/flask_app/main.py
+++ projects/7/flask_app/main.py
@@ -1,6 +1,63 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
 
-@app.route('/')
+app = Flask(__name__, template_folder='template')
+url = 'https://kolesa.kz/mototehnika/'
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                         "Chrome/77.0.3865.90 Safari/537.36"}
+
+@app.route("/login/")
+def login():
+    return render_template('login.html')
+
+
+@app.route("/home/")
+def home():
+    users = [{"name": f"Alice ({x + 18})", "age": x + 18} for x in range(1, 100 + 1)]
+    print(users)
+
+    user = {"name": f"Alice (18)", "age": 18}
+
+    return render_template('home.html', price=666, user=user, users=users)
+
+
+@app.route("/")
 def hello_world():
-    return 'Hello, World!'
+    response = requests.get(url=url, headers=headers)
+    data1 = response.text
+    bs4Obj = BeautifulSoup(data1, 'html.parser')
+
+    class Vehicle:
+        def __init__(self, price, city, model, description, url, name, phone):
+            self.phone = phone
+            self.name = name
+            self.url = url
+            self.model = model
+            self.description = description
+            self.city = city
+            self.price = price
+
+    objects = bs4Obj.find_all('div', class_="a-list__item")  # вся техника!
+    print(len(objects))
+
+    lists = ""
+    for obj in objects:
+        try:
+            # вытаскиваем цену одного транспорта
+            price = int(
+                ''.join(str(obj.find('span', class_="a-card__price")).split('>')[1].split('<')[0].strip().split()))
+
+            # вытаскиваем модель одного транспорта
+            # model = str(''.join(str(obj.find('a', class_="a-card__link")).split('>')[1].split('<')[0].strip().split()))
+            model = str(obj.find('h5', class_="a-card__title")).split('</a>')[0].split('target="_blank">')[1].strip()
+            print(f"{model}: {price}")
+            print('\n\n\n*********************************************************\n\n\n')
+            lists += f"<li><strong>{model}</strong>: {price}</li>"
+        except Exception as error:
+            pass
+    # sep1 = 'class="a-list"'
+    # data2 = data1.split(sep=sep1)[1]
+    # print(data2)
+
+    return f"<ul>{lists}</ul>"
---
+++ projects/7/flask_app/start_app.cmd
@@ -0,0 +1,3 @@
+call env/Scripts/activate.bat
+flask --app main --debug run --host=0.0.0.0 --port=8000
+cmd
